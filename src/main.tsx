bea3667e8a664aba2ec4bd382b47a62b88fb514f
Preyash Patel <preyashpatel@PreyashsMacbook.home>
2023-04-11 21:24:05 -0400

    (minor update) pocketbase trial

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { MantineProvider } from "@mantine/core";
+import PocketBase from "pocketbase";
+const pock = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
+
+console.log(pock.health.client);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
