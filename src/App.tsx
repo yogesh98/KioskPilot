10c58b57a1035b811036a1635450f0e09c0d122e
Preyash Patel <preyashpatel@PreyashsMacbook.home>
2023-04-11 21:07:46 -0400

    (setup) added pocketbase, mantine, react-router-dom and cleaned out files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Alert, Box, Button } from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [display, show] = useState(false);
+
+  const onClick = () => {
+    show(!display);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Box
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Button
+        onClick={onClick}
+        style={{
+          width: "10%",
+          marginBottom: "1rem",
+        }}
+      >
+        Show
+      </Button>
+      {display ? (
+        <Alert title="👋" color="green">
+          Hello World!
+        </Alert>
+      ) : null}
+    </Box>
+  );
 }
 
-export default App
+export default App;
