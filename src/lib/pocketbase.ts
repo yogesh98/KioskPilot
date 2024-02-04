3945f3ad95c731765eb16c0b81f10d833b011a97
Yogesh Patel <me@yogeshp.dev>
2024-02-04 02:01:15 -0500

    (Update) delete everything

--- src/lib/pocketbase.ts
+++
@@ -1,9 +0,0 @@
-import PocketBase from "pocketbase";
-
-const pb: PocketBase = new PocketBase(import.meta.env.VITE_POCKETBASE_URL);
-await pb.admins.authWithPassword(
-  import.meta.env.VITE_USER_ID,
-  import.meta.env.VITE_PASSWORD
-);
-
-export default pb;
