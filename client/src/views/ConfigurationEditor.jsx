cc78ceb0ac02af8385110a01428bcba63f2548c8
Yogesh Patel <me@yogeshp.dev>
2024-02-09 00:41:23 -0500

    (upadte) nothing

--- client/src/views/ConfigurationEditor.jsx
+++ client/src/views/ConfigurationEditor.jsx
@@ -1,36 +1,14 @@
 import React, { useEffect, useState, useCallback } from "react";
 import { Box, Flex, Image, Spinner, Stack, Text, useColorModeValue } from '@chakra-ui/react'
-import GridLayout from "react-grid-layout";
-
-import 'react-grid-layout/css/styles.css' 
-import 'react-resizable/css/styles.css' 
-
-function CueDraggablePiece(props) {
-    return (
-        <Flex
-            w="100%"
-            h="100%"
-            justifyContent="center"
-            alignItems="center"
-            borderWidth={useColorModeValue('2px', '')}
-            borderRadius="lg"
-            boxShadow={useColorModeValue('lg', 'dark-lg')}
-            bg={useColorModeValue('gray.100', 'gray.900')}
-            direction={'column'}
-        >
-            <Text fontSize={{ base: '14px', md: '20px', lg: '112x' }}>{props.description}</Text>
-            <Text fontSize={{ base: '10px', md: '15px', lg: '100x' }}>{props.title}</Text>
-        </Flex>
-    );
-}
 
 export default function ConfigurationEditor() {
-    const [loading, setLoading] = useState(true);
-    const [layout, setLayout] = useState([]);
-
-    const onLayoutChange = (l) => {
-        setLayout(l);
+    const widget_common_styles = {
+        "borderRadius": '15px',
+        "borderColor": useColorModeValue('gray.200', 'whitealpha.300'),
+        "borderWidth": 4,
+        "bg": useColorModeValue('gray.100', 'gray.900'),
     }
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         setLoading(false)
@@ -57,47 +35,28 @@ export default function ConfigurationEditor() {
     <>
     <Flex 
         borderRadius={'15px'} 
-        borderColor={useColorModeValue('gray.200', 'whitealpha.300')} 
-        borderWidth={4}
-        bg={useColorModeValue('gray.100', 'gray.900')}
-        // minH={'100%'}
+        minH={'100%'}
     >
         <Stack
             overflowY={'scroll'}
-            borderRightWidth={4}
-            minWidth={'120px'}
+            minWidth={'200px'}
         >
-            <Box>
-                Drag and Drop box akls;djfalkjfakl;jldkf
+            <Box
+                flexGrow={1}
+                {...widget_common_styles}
+            >
+                
+            </Box>
+
+            <Box
+                flexGrow={1}
+                {...widget_common_styles}
+            >
+                
             </Box>
         </Stack>
         <Box id="layoutContainer">
-            <div
-                // style={{transform: 'scale(0.5) translate(-50%, -50%)'}}
-            >
-                <GridLayout
-                    cols={20}
-                    rowHeight={30}
-                    width={1080}
-                    layout={layout}
-                    onLayoutChange={(layout) => onLayoutChange(layout)}
-                    style={{
-                        height: '1920px',
-                        width: '1080px',
-                        overflowX: 'hidden',
-                        overflowY: 'auto',
-                    }}
-                    allowOverlap={true}
-                    // transformScale={.5}
-                >
-                    <Box key={1}>
-                        <span>Hello</span>
-                    </Box>
-                    <Box key={2}>
-                        <span>Hello2</span>
-                    </Box>
-                </GridLayout> 
-            </div>
+
         </Box>
     </Flex>
     </>
