8de5a0fe289be4138a96c4e7921a3e153b57874e
Yogesh Patel <yogesh@inquisithealth.com>
2024-03-26 21:15:05 -0400

    (Upate) config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  esbuild: {
+    // Including JS files explicitly for the build
+    include: /.*\.(js|jsx)$/,
+  },
+});
